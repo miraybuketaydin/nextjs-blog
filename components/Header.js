ff592ccdff54862cf54efbcb1ff8c9c1622b990a
Buket Miray <miraybuketaydin@gmail.com>
2023-02-09 15:00:41 +0300

    next upgrade

--- components/Header.js
+++ components/Header.js
@@ -2,24 +2,35 @@ import Link from "next/link";
 import styled from "styled-components";
 
 const HeaderDiv = styled.div`
-  color: #f00;
   border: 10px solid #f00;
   padding: 30px;
   background-color: #f00;
+  display: flex;
+  gap: 20px;
+
+  & > span {
+    font-size: 30px;
+  }
+`;
+
+const StyledA = styled.a`
+  color: #fff;
+  text-decoration: none;
+  border: ${({ borderSize }) => `${borderSize}px`} solid yellow;
+  font-size: ${({ big }) => (big ? "20px" : "12px")};
+  &:hover {
+    text-decoration: underline;
+  }
 `;
 
 export default () => (
-  <div>
-    Header Componenti başlangıcı
-    <HeaderDiv>
-      <a href="/" passHref>
-        Ana Sayfa
-      </a>
-    </HeaderDiv>
-    <div>
-      <a href="/about" passHref>
-        Hakkımızda
-      </a>
-    </div>
-  </div>
+  <HeaderDiv>
+    <StyledA href="/" passHref borderSize={20}>
+      Ana Sayfa
+    </StyledA>
+    <StyledA href="/about" passHref borderSize={10} big>
+      Hakkımızda
+    </StyledA>
+    <span>Ek Açıklamalar</span>
+  </HeaderDiv>
 );
